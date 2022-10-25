3f327b814b03533dea0aab14aace7dc5f75930e8
Vladimir <veliseykin2000@gmail.com>
2022-10-25 13:41:41 -0700

    Footer

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass('sticky');
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass('show');
+        }else{
+            $('.scroll-up-btn').removeClass('show');
+        }
     })
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
@@ -12,3 +17,24 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     })
 })
+
+// typing animation script
+
+var typed = new Typed('.typing',{
+    strings: ['Software Engeenier', 'Full Stack Developer', 'Back-End Engeenier'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+var typed = new Typed('.typing-2',{
+    strings: ['Software Engeenier', 'Full Stack Developer', 'Back-End Engeenier'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+// Slid-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+});
